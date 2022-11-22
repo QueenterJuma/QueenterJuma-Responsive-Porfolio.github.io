ef0b9e371250140ea0a63d9af17b794aba3cffd6
QueenterJuma <jumahqueenter@gmail.com>
2022-11-22 16:57:07 +0300

    Fix linter errors

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const toggleMenu = document.querySelector(".toggle-menu");
-const navigation = document.querySelector(".navigation");
+const toggleMenu = document.querySelector('.toggle-menu');
+const navigation = document.querySelector('.navigation');
 let menuOpen = false;
 
-toggleMenu.addEventListener("click", () => {
+toggleMenu.addEventListener('click', () => {
   if (!menuOpen) {
-    toggleMenu.classList.add("open");
+    toggleMenu.classList.add('open');
     menuOpen = true;
   } else {
-    toggleMenu.classList.remove("open");
+    toggleMenu.classList.remove('open');
     menuOpen = false;
   }
-  navigation.classList.toggle("open");
+  navigation.classList.toggle('open');
 });
