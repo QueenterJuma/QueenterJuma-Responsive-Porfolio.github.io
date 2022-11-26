85c1ffba18c525156ada12131f6849de49e0d92d
QueenterJuma <jumahqueenter@gmail.com>
2022-11-26 23:00:21 +0300

    add changes in script.js

--- script.js
+++ script.js
@@ -1,127 +1,74 @@
-const toggleMenu = document.querySelector(".toggle-menu");
-const navigation = document.querySelector(".navigation");
+const toggleMenu = document.querySelector('.toggle-menu');
+const navigation = document.querySelector('.navigation');
+let counterImage = 0;
 let menuOpen = false;
-toggleMenu.addEventListener("click", () => {
-  if (!menuOpen) {
-    toggleMenu.classList.add("open");
-    menuOpen = true;
-  } else {
-    toggleMenu.classList.remove("open");
-    menuOpen = false;
-  }
-  navigation.classList.toggle("open");
-  return menuOpen;
-});
-const reviewPage = document.querySelectorAll(".nav-item");
-for (let i = 0; i < reviewPage.length; i += 1) {
-  reviewPage[i].addEventListener("click", () => {
-    toggleMenu.classList.toggle("open");
-    navigation.classList.toggle("open");
-  });
-}
 
 /*popup-window*/
 
 const data = [
   {
-    id: "modal",
-    popup_title: "Tonic",
-    popup_canopy_section: ["CANOPY", "Back End Dev", 2015],
-    popup_image1: "dec1.png",
+    id: 'modal',
+    popup_title: 'Tonic',
+    popup_canopy_section: ['CANOPY', 'Back End Dev', 2015],
+    popup_image1: 'dec3.png',
     popup_desc1:
-      "A daily selection of privately personalized reads; no accounts or sign-ups required",
+      'A daily selection of privately personalized reads; no accounts or sign-ups required',
     popup_description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-    popup_tags: ["html", "css", "javascript"],
-    see_live: "#",
-    see_source: "#",
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    popup_tags: ['html', 'css', 'javascript'],
+    see_live: '#',
+    see_source: '#',
   },
 
   {
-    id: "modal",
-    popup_title: "Multi-Post Stories",
-    popup_canopy_section: ["CANOPY", "Back End Dev", 2015],
-    popup_image1: "dec2.png",
+    id: 'modal',
+    popup_title: 'Multi-Post Stories',
+    popup_canopy_section: ['CANOPY', 'Back End Dev', 2015],
+    popup_image1: 'dec2.png',
     popup_desc1:
-      "A daily selection of privately personalized reads; no accounts or sign-ups required",
+      'A daily selection of privately personalized reads; no accounts or sign-ups required',
     popup_description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-    popup_tags: ["html", "css", "javascript"],
-    see_live: "#",
-    see_source: "#",
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    popup_tags: ['html', 'css', 'javascript'],
+    see_live: '#',
+    see_source: '#',
   },
 
   {
-    id: "modal",
-    popup_title: "Tonic",
-    popup_canopy_section: ["CANOPY", "Back End Dev", 2015],
-    popup_image1: "dec3.png",
+    id: 'modal',
+    popup_title: 'Tonic',
+    popup_canopy_section: ['CANOPY', 'Back End Dev', 2015],
+    popup_image1: 'dec1.png',
     popup_desc1:
-      "A daily selection of privately personalized reads; no accounts or sign-ups required",
+      'A daily selection of privately personalized reads; no accounts or sign-ups required',
     popup_description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-    popup_tags: ["html", "css", "javascript"],
-    see_live: "#",
-    see_source: "#",
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    popup_tags: ['html', 'css', 'javascript'],
+    see_live: '#',
+    see_source: '#',
   },
 
   {
-    id: "modal",
-    popup_title: "Multi-Post Stories",
-    popup_canopy_section: ["CANOPY", "Back End Dev", 2015],
-    popup_image1: "dec4.png",
+    id: 'modal',
+    popup_title: 'Multi-Post Stories',
+    popup_canopy_section: ['CANOPY', 'Back End Dev', 2015],
+    popup_image1: 'dec4.png',
     popup_desc1:
-      "A daily selection of privately personalized reads; no accounts or sign-ups required",
+      'A daily selection of privately personalized reads; no accounts or sign-ups required',
     popup_description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-    popup_tags: ["html", "css", "javascript"],
-    see_live: "#",
-    see_source: "#",
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    popup_tags: ['html', 'css', 'javascript'],
+    see_live: '#',
+    see_source: '#',
   },
 ];
-/*let container = document.getElementById("popup-card-container");
-
-container.innerHTML = `<div class='popup-card-header' id='popup-card-header'>
-    <div class='popup-header-title-holder' id='popup-header-title-holder'>
-    <h2 class='popup-title' id='popup_title'>Tonic</h2>
-    <div class='popup-btn-span'>
-    <img src='./image/Icon_Cancel.png' alt='cancel popup' class='cancel-popup'>
-    </div>
-    </div>
-    <ul class='popup-canopy-section' id='popup_canopy_section'>
-    <li>CANOPY</li>
-    <li>Back End Dev</li>
-    <li>2015</li>
-    </ul>
-    </div>
-    <img src='./image/popup_image.png' alt='popup image' class='popup-image' id='popup_image1'>
-    <div class='popup-left-block'>
-    <p class='popup-description' id='popup_description'>
-      ${data[0].popup_description}
-    </p>
-    <ul class='popup-tags' id='popup_tags'>
-    <li>html</li>
-    <li>css</li>
-    <li>javascript</li>
-    </ul>
-    <div class='popup-divider'>
-    <div class='popup-action-frame'>
-      <div>
-      <button class='popup-left-btn' id='see_live'> See live</button>
-      <a href='#'><img src='./image/demo_icon.png' alt='demo-icon' /></a>
-      </div>
-      <div >
-      <button class='popup-right-btn' id='see_source'>See source</button>
-      <a class='popup-giticon' id='git-icon'href='#'><i class='fa-brands fa-github'></i></a>
-      </div>
-    </div>
-    </div>`*/
 
 for (key in data) {
+  
   document.getElementById(
-    "portfolio"
+    'portfolio'
   ).innerHTML += `<div class="d-flex-column card one">
-      <div class="plc-img1  images" > 
+      <div class="plc-img${counterImage}  images" > 
       </div>
       <div class="card-text">
         <h3>${data[key].popup_title}</h3>
@@ -138,25 +85,63 @@ for (key in data) {
           <li><a href="">${data[key].popup_tags[1]}</a></li>
           <li><a href="">${data[key].popup_tags[2]}</a></li>
         </ul>
-        <button onclick = "openModal()">See Project</button>
+        <button onclick = "openModal('${counterImage}')"><a href="#">See Project<a></button>
       </div>
     </div>`;
+  counterImage += 1;
 }
 
 const openModal = (portfolio) => {
-  popup_title.innerHTML = data[portfolio].popup_title;
-  popup_canopy_section.innerHTML = data[portfolio].popup_canopy_section;
-  popup_image1.innerHTML = data[portfolio].popup_image1;
-  popup_desc1.innerHTML = data[portfolio].popup_desc1;
-  popup_description.innerHTML = data[portfolio].popup_description;
-  popup_tags.innerHTML = data[portfolio].popup_tags;
-  see_live.innerHTML = data[portfolio].see_live;
-  see_source.innerHTML = data[portfolio].see_source;
+  document.getElementById('top_nav').style.filter = 'blur(5px)';
+  document.getElementById('main-cont').style.filter = 'blur(5px)';
+  document.getElementById('modal').style.display = 'block';
+  document.getElementById(
+    'modal'
+  ).innerHTML = ` <div class='popup-card-header' id='popup-card-header'>
+        <div class='popup-header-title-holder' id='popup-header-title-holder'>
+        <h2 class='popup-title' id='popup_title'>Tonic</h2>
+        <div class='popup-btn-span'>
+        <img src='./image/Icon_Cancel.png' alt='cancel popup' class='cancel-popup' onclick="closeModal()">
+        </div>
+        </div>
+        <ul class='popup-canopy-section' id='popup_canopy_section'>
+        <li>CANOPY</li>
+        <li><img src="./image/Counter.png" alt="counter dot"></li>
+        <li>Back End Dev</li>
+        <li><img src="./image/Counter.png" alt="counter dot"></li>
+        <li>2015</li>
+        </ul>
+        </div>
+        <img src='./image/${data[portfolio].popup_image1}' alt='popup image' class='popup-image' id='popup_image1'>
+        <div class='popup-left-block'>
+        <p class='popup-description' id='popup_description'>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent
+        
+        </p>
+        <ul class='popup-tags' id='popup_tags'>
+        <li>html</li>
+        <li>css</li>
+        <li>javascript</li>
+        </ul>
+        <div class='popup-divider'>
+        <div class='popup-action-frame' id='popbtn'>
+          <span class='link_btn_container'>
+          <button class='popup-left-btn' id='see_live'>See live <a class='popup-giticon' id='git-icon'href='#'><img src='./image/demo_icon.png' alt='demo-icon' /></a></button>
+          
+          </span>
+          <span class='link_btn_container'>
+          <button class='popup-right-btn' id='see_source'><p class="see">See source</p> <a class='popup-giticon' id='git-icon'href='#'><i class='fa-brands fa-github'></i></a></button>
+          
+          </span>
+        </div>
+        </div>
+
+        </div>
+        `;
 };
 
 const closeModal = () => {
-  openModal.style.display = "none";
-  body.style.overflow = "auto";
+  document.getElementById('modal').style.display = 'none';
+  document.getElementById('top_nav').style.filter = 'blur(0px)';
+  document.getElementById('main-cont').style.filter = 'blur(0px)';
 };
-openModal();
-closeModal();
